11329846710335bdc71e6e375e272bf4547abbc7
prounknown055-pixel <prounknown055@gmail.com>
2026-01-19 05:36:59 +0530

    Fix import path for Tournament and RegistrationStatus

--- src/TournamentCard.tsx
+++ src/TournamentCard.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Tournament, RegistrationStatus } from '../types';
+import { Tournament, RegistrationStatus } from './types';
 import { ASSETS } from './constants';
 
 interface TournamentCardProps {
