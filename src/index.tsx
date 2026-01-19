8c4100775329c84fdf05ec6dfc54bc6ebf251e45
prounknown055-pixel <prounknown055@gmail.com>
2026-01-19 19:47:44 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '../index.css';
+import './index.css';
 
 const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
 
-if (!rootElement) {
-  throw new Error('Could not find root element to mount to');
-}
-
-ReactDOM.createRoot(rootElement).render(
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
