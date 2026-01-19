53e39a0a8b65a9549849046ed3ff94eb012e8168
prounknown055-pixel <prounknown055@gmail.com>
2026-01-19 19:49:36 +0530

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://bhmojnzgcinuglohzabq.supabase.co';
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJobW9qbnpnY2ludWdsb2h6YWJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4MTY4ODAsImV4cCI6MjA4NDM5Mjg4MH0.xalJA4GAt0DZ9LFfH6abWeCxhm8PoGmVgoV3BWGVxXk';
+const supabaseAnonKey = 'sb_publishable_tiUgp4_-CvxSEU5GH0k8pw_HEawHV3o';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
