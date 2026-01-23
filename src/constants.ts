8a44a8a5189649864d27dd00a936b22c0ab058f0
prounknown055-pixel <prounknown055@gmail.com>
2026-01-23 13:18:02 +0530

    Delete src/constants.ts

--- src/constants.ts
+++
@@ -1,9 +0,0 @@
-export const ADMIN_EMAIL = 'tournamentsakamao@gmail.com';
-export const ADMIN_PASSWORD = 'musicstudio250@gmail.com';
-
-export const ASSETS = {
-  officialLogo: 'https://i.postimg.co/85pL6j7V/at-logo-official.png', 
-  bgMusic: '/bgm.mp3',   
-  clickSound: '/tap.mp3',
-  qrPlaceholder: 'https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=upi://pay?pa=tournamentsakamao@upi'
-};
