28c55766068c39548c594f990c6805ba5170c5d6
prounknown055-pixel <prounknown055@gmail.com>
2026-01-23 15:10:11 +0530

    Update AdminPanel.tsx

--- src/components/AdminPanel.tsx
+++ src/components/AdminPanel.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { supabase } from '../supabaseClient';
 import { Tournament, Registration, RegistrationStatus, GameType, ChatMessage, AppSettings, WithdrawalRequest } from '../types';
 import { GAMES, ASSETS } from '../constants';
 
